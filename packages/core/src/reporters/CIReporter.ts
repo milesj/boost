fe33ca4a252e3e0d2d7cf8608a6a936bf9c438c0
Miles Johnson <mileswjohnson@gmail.com>
2018-08-31 19:48:37 -0700

    Fix CI colors.

--- packages/core/src/reporters/CIReporter.ts
+++ packages/core/src/reporters/CIReporter.ts
@@ -3,15 +3,19 @@
  * @license     https://opensource.org/licenses/MIT
  */
 
+import chalk from 'chalk';
 import Reporter from '../Reporter';
 import Task from '../Task';
 import Routine from '../Routine';
+import { Color } from '../types';
 
-export type TaskID = {
-  id?: number;
-};
+const COLORS: Color[] = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white', 'gray'];
 
 export default class CIReporter extends Reporter {
+  colorIndex: number = -1;
+
+  ids: Map<Task<any>, number> = new Map();
+
   taskID: number = 0;
 
   bootstrap() {
@@ -27,35 +31,66 @@ export default class CIReporter extends Reporter {
       .on('routine.fail', this.handleRoutineFail);
   }
 
-  handleTask = (task: Task<any> & TaskID) => {
-    // eslint-disable-next-line no-param-reassign
-    task.id = this.taskID;
-    this.taskID += 1;
-
-    this.console.out(`[${task.id}] Running task: ${task.title}\n`);
+  handleTask = (task: Task<any>) => {
+    this.console.out(
+      `${this.cycleStyle(`[${this.getTaskID(task)}]`)} Running task: ${task.title}\n`,
+    );
   };
 
-  handleTaskPass = (task: Task<any> & TaskID) => {
-    this.console.out(`${this.style(`[${task.id}] Passed`, 'success')}\n`);
+  handleTaskPass = (task: Task<any>) => {
+    this.console.out(
+      `${this.cycleStyle(`[${this.getTaskID(task)}]`)} ${this.style('Passed', 'success')}\n`,
+    );
   };
 
-  handleTaskFail = (task: Task<any> & TaskID, error: Error) => {
-    this.console.err(`${this.style(`[${task.id}] Failed: ${error.message}`, 'failure')}\n`);
+  handleTaskFail = (task: Task<any>, error: Error) => {
+    this.console.err(
+      `${this.cycleStyle(`[${this.getTaskID(task)}]`)} ${this.style('Failed:', 'failure')} ${
+        error.message
+      }\n`,
+    );
   };
 
   handleRoutine = (routine: Routine<any>) => {
-    this.console.out(`[${routine.key}] Running routine: ${routine.title}\n`);
+    this.console.out(`${this.cycleStyle(`[${routine.key}]`)} Running routine: ${routine.title}\n`);
   };
 
   handleRoutinePass = (routine: Routine<any>) => {
-    this.console.out(`${this.style(`[${routine.key}] Passed`, 'success')}\n`);
+    this.console.out(`${this.cycleStyle(`[${routine.key}]`)} ${this.style('Passed', 'success')}\n`);
   };
 
   handleRoutineFail = (routine: Routine<any>, error: Error) => {
-    this.console.err(`${this.style(`[${routine.key}] Failed: ${error.message}`, 'failure')}\n`);
+    this.console.err(
+      `${this.cycleStyle(`[${routine.key}]`)} ${this.style('Failed:', 'failure')} ${
+        error.message
+      }\n`,
+    );
   };
 
   handleStop = () => {
     this.console.out(`Ran in ${this.getElapsedTime(this.startTime, this.stopTime, false)}\n`);
   };
+
+  getTaskID(task: Task<any>): number {
+    let id;
+
+    if (this.ids.has(task)) {
+      id = this.ids.get(task)!;
+    } else {
+      id = this.taskID;
+      this.taskID += 1;
+    }
+
+    return id;
+  }
+
+  cycleStyle(message: string): string {
+    this.colorIndex += 1;
+
+    if (this.colorIndex === COLORS.length) {
+      this.colorIndex = 0;
+    }
+
+    return chalk[COLORS[this.colorIndex]](message);
+  }
 }
