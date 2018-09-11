dadffad02635b760efebe27d56f90e4ef7176001
Miles Johnson <mileswjohnson@gmail.com>
2018-09-10 20:12:50 -0700

    Update initial setup.

---
+++ scripts/build-packages.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+root=$PWD
+
+# We can't use Beemo to build our packages with TypeScript,
+# as Beemo's CLI requires Boost itself.
+
+build_pkg() {
+  echo "$1"
+  cp "$root/scripts/build-tsconfig.json" "$root/$1/tsconfig.json"
+  cd "$root/$1" || exit
+  node ../../node_modules/.bin/tsc
+}
+
+build_pkg "./packages/core"
+cd "$root" || exit
+
+for pkg in ./packages/*; do
+  if [ "$pkg" != "./packages/core" ]
+  then
+    build_pkg "$pkg"
+  fi
+done
