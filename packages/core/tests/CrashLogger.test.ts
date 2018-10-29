d0ffd50142ddf7111508c5292f656cd6c201f4f8
Miles Johnson <mileswjohnson@gmail.com>
2018-10-29 15:53:05 -0700

    Redo mocks.

--- packages/core/tests/CrashLogger.test.ts
+++ packages/core/tests/CrashLogger.test.ts
@@ -4,21 +4,29 @@ import CrashLogger from '../src/CrashLogger';
 import Tool from '../src/Tool';
 import { createTestTool } from './helpers';
 
-jest.mock('fs');
-
 describe('CrashLogger', () => {
   let tool: Tool<any, any>;
   let logger: CrashLogger;
+  let spy: jest.Mock;
+
+  const oldWrite = fs.writeFileSync.bind(fs);
 
   beforeEach(() => {
     tool = createTestTool();
     logger = new CrashLogger(tool);
+    spy = jest.fn();
+
+    fs.writeFileSync = spy;
+  });
+
+  afterEach(() => {
+    fs.writeFileSync = oldWrite;
   });
 
   it('writes to a log file', () => {
     logger.log(new Error());
 
-    expect(fs.writeFileSync).toHaveBeenCalledWith(
+    expect(spy).toHaveBeenCalledWith(
       path.join(tool.options.root, 'test-boost-error.log'),
       expect.anything(),
       'utf8',
@@ -31,7 +39,7 @@ describe('CrashLogger', () => {
 
     logger.log(new Error());
 
-    expect(fs.writeFileSync).toHaveBeenCalledWith(
+    expect(spy).toHaveBeenCalledWith(
       path.join(tool.options.root, 'test-boost-error.log'),
       expect.anything(),
       'utf8',
