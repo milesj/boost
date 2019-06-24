7a2f0d663b959cccbb0466389a5cf8c44aea8f5d
Miles Johnson <mileswjohnson@gmail.com>
2019-06-23 19:23:31 -0700

    Handle routines.

--- packages/pipeline/src/Pipeline.ts
+++ packages/pipeline/src/Pipeline.ts
@@ -1,21 +1,28 @@
-/* eslint-disable no-magic-numbers, lines-between-class-members, no-dupe-class-members */
+/* eslint-disable */
 
 import Context from './Context';
+import Routine from './Routine';
 import Task from './Task';
 import WorkUnit from './WorkUnit';
 import { Action, Runnable } from './types';
 
-class Pipeline<Ctx extends Context, Input = unknown> {
-  parent?: Pipeline<Ctx, any>;
+class Pipeline<Input = unknown, Ctx extends Context = Context> {
+  parent?: Pipeline<any>;
 
   work?: Runnable<Input, any>;
 
-  pipe<Output = unknown>(title: string, action: Action<Ctx, Input, Output>): Pipeline<Ctx, Output>;
-  pipe<Output = unknown>(workUnit: Runnable<Input, Output>): Pipeline<Ctx, Output>;
+  value?: Input;
+
+  constructor(initialValue?: Input) {
+    this.value = initialValue;
+  }
+
+  pipe<Output = unknown>(title: string, action: Action<Ctx, Input, Output>): Pipeline<Output>;
+  pipe<Output = unknown>(workUnit: Runnable<Input, Output>): Pipeline<Output>;
   pipe<Output = unknown>(
     titleOrWorkUnit: string | Runnable<Input, Output>,
     action?: Action<Ctx, Input, Output>,
-  ): Pipeline<Ctx, Output> {
+  ): Pipeline<Output> {
     if (titleOrWorkUnit instanceof WorkUnit) {
       this.work = titleOrWorkUnit;
     } else if (typeof titleOrWorkUnit === 'string' && typeof action === 'function') {
@@ -24,19 +31,19 @@ class Pipeline<Ctx extends Context, Input = unknown> {
       throw new TypeError('Unknown work unit type. Must be a `Routine` or `Task`.');
     }
 
-    const pipeline = new Pipeline<Ctx, Output>();
+    const pipeline = new Pipeline<Output>();
     pipeline.parent = this;
 
     return pipeline;
   }
 
   run<Result = unknown>(): Result {
-    return '';
+    return this.value;
   }
 }
 
 // Implicit tasks
-new Pipeline<Context, number>()
+new Pipeline(0)
   .pipe(
     'one',
     (c, n) => n + 123,
@@ -52,8 +59,45 @@ new Pipeline<Context, number>()
   .run();
 
 // Explicit tasks
-new Pipeline<Context, number>()
+new Pipeline(0)
   .pipe(new Task('one', (c, n) => n + 123))
   .pipe(new Task('two', (c, n) => String(n)))
   .pipe(new Task('three', (c, s) => s.toUpperCase()))
+  .run();
+
+// Routines
+class One extends Routine<{}, number, number> {
+  blueprint() {
+    return {};
+  }
+
+  async execute(c: Context, n: number): Promise<number> {
+    return n + 123;
+  }
+}
+
+class Two extends Routine<{}, number, string> {
+  blueprint() {
+    return {};
+  }
+
+  async execute(c: Context, n: number): Promise<string> {
+    return String(n);
+  }
+}
+
+class Three extends Routine<{}, string, string> {
+  blueprint() {
+    return {};
+  }
+
+  async execute(c: Context, s: string): Promise<string> {
+    return s.toUpperCase();
+  }
+}
+
+new Pipeline(0)
+  .pipe(new One('one', 'One'))
+  .pipe(new Two('two', 'Two'))
+  .pipe(new Three('three', 'Three'))
   .run();
