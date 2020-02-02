f16efb3d4f33aa4c10f83763bbb8e5b067b437b8
Miles Johnson <mileswjohnson@gmail.com>
2020-02-01 17:33:15 -0800

    build: Add release requirements script.

---
+++ scripts/checkReleaseRequirements.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+
+const chalk = require('chalk');
+
+if (!process.env.GH_TOKEN) {
+  console.error(chalk.red('Release requires a GH_TOKEN environment variable.'));
+  process.exit(1);
+}
