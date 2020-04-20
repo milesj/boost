64206161cefc127ee33f56b87729d525d4e1d995
Miles Johnson <mileswjohnson@gmail.com>
2020-04-19 17:01:49 -0700

    Delete demos.

--- demos/multi-reporter/configs/demosMultiReporter.js
+++
@@ -1,5 +0,0 @@
-const BoostReporter = require('../../../packages/core/lib/reporters/BoostReporter').default;
-
-module.exports = {
-  reporters: [new BoostReporter(), new BoostReporter(), new BoostReporter()],
-};
--- demos/random.js
+++
@@ -1,3 +0,0 @@
-module.exports = function random(max = 10, min = 3) {
-  return Math.floor(Math.random() * max) + min;
-};
