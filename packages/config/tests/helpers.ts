b191e13b4b8e442050e1b1e9c1e66055e84714df
Miles Johnson <mileswjohnson@gmail.com>
2020-07-26 17:36:36 -0700

    tests: Fix failing tests after recent `Path` changes.

--- packages/config/tests/helpers.ts
+++ packages/config/tests/helpers.ts
@@ -1,12 +1,5 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-
 import { Path } from '@boost/common';
 
 export function stubPath(part: string, wrap: boolean = true) {
-  const path = process.platform === 'win32' && wrap ? new Path('D:', part) : new Path(part);
-
-  // @ts-expect-error
-  path.stats = expect.any(Object);
-
-  return path;
+  return process.platform === 'win32' && wrap ? new Path('D:', part) : new Path(part);
 }
