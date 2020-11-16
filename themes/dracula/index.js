a3669958da9e128bf7aad5195a9d263c9d04c396
Miles Johnson <mileswjohnson@gmail.com>
2020-11-15 18:12:54 -0800

    Update themes.

--- themes/dracula/index.js
+++ themes/dracula/index.js
@@ -6,8 +6,10 @@
 module.exports = {
   default: '#f8f8f2',
   failure: '#ff5555',
+  info: '#9ee4f7',
   inverted: '#282a36',
   muted: '#999999',
+  notice: '#e97fbf',
   success: '#50fa7b',
   warning: '#f1fa8c',
 };
--- themes/moon-dark/index.js
+++ themes/moon-dark/index.js
@@ -6,8 +6,10 @@
 module.exports = {
   default: '#e2e2e2',
   failure: '#e88147',
+  info: '#47c1d6',
   inverted: '#201d1e',
-  muted: '#83abbd',
-  success: '#bf67d9',
+  muted: '#7C7C7C',
+  notice: '#b368db',
+  success: '#7bb571',
   warning: '#fbb71b',
 };
