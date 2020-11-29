9809518fb6ae19db8e30c7595c71edb9f720a594
Miles Johnson <mileswjohnson@gmail.com>
2020-11-28 17:24:55 -0800

    Update deps.

--- packages/cli/tests/Command2.test.tsx
+++ packages/cli/tests/Command2.test.tsx
@@ -3,14 +3,6 @@ import { Box, Text } from 'ink';
 import Command from '../src/Command2';
 
 class BoostCommand extends Command {
-  static description = 'Description';
-
-  static path = 'boost';
-
-  static allowUnknownOptions = true;
-
-  static allowVariadicParams = true;
-
   run() {
     return Promise.resolve();
   }
