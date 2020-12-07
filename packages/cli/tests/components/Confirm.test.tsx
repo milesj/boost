08aabad367fcc6c61447dd4d71d71849986371ba
Miles Johnson <mileswjohnson@gmail.com>
2020-12-06 18:29:19 -0800

    fix(Confirm): Log errors to the console instead of exiting.

--- packages/cli/tests/components/Confirm.test.tsx
+++ packages/cli/tests/components/Confirm.test.tsx
@@ -20,6 +20,30 @@ describe('Confirm', () => {
     expect(lastFrame()).toMatchSnapshot();
   });
 
+  it('console errors if `yes` is invalid', async () => {
+    const spy = jest.spyOn(console, 'error').mockImplementation();
+
+    render(<Confirm {...props} yes="" />);
+
+    await delay();
+
+    expect(spy).toHaveBeenCalledWith('Prop "yes" must be a single character.');
+
+    spy.mockRestore();
+  });
+
+  it('console errors if `no` is invalid', async () => {
+    const spy = jest.spyOn(console, 'error').mockImplementation();
+
+    render(<Confirm {...props} no="" />);
+
+    await delay();
+
+    expect(spy).toHaveBeenCalledWith('Prop "no" must be a single character.');
+
+    spy.mockRestore();
+  });
+
   it('errors for invalid input value', async () => {
     const { lastFrame, stdin } = render(<Confirm {...props} />);
 
