e7d767c326f0c0524cdc2d609d54f451c121455b
Miles Johnson <mileswjohnson@gmail.com>
2020-12-06 19:38:02 -0800

    new: Add `useRenderLoop` hook.

--- packages/cli/src/hooks/index.ts
+++ packages/cli/src/hooks/index.ts
@@ -2,3 +2,4 @@ export * from './useDimensions';
 export * from './useIsMounted';
 export * from './useListNavigation';
 export * from './useProgram';
+export * from './useRenderLoop';
---
+++ packages/cli/src/hooks/useRenderLoop.ts
@@ -0,0 +1,23 @@
+import { useCallback, useEffect, useReducer, useRef } from 'react';
+
+// eslint-disable-next-line no-magic-numbers
+const DEFAULT_FPS = 1000 / 30;
+
+export function useRenderLoop(fps: number = DEFAULT_FPS): () => void {
+  const [, forceUpdate] = useReducer((count) => count + 1, 0);
+  const timer = useRef<NodeJS.Timeout>();
+
+  const clear = useCallback(() => {
+    if (timer.current) {
+      clearInterval(timer.current);
+    }
+  }, []);
+
+  useEffect(() => {
+    timer.current = setInterval(forceUpdate, fps);
+
+    return clear;
+  }, [clear, fps]);
+
+  return clear;
+}
