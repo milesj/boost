fdde450ac902959217feaafcbbabf4a9aeb36613
Miles Johnson <mileswjohnson@gmail.com>
2021-02-05 21:14:41 -0800

    deps(json5): Updated to v2.2.

--- packages/common/src/serializers/json.ts
+++ packages/common/src/serializers/json.ts
@@ -1,19 +1,17 @@
 import JSON from 'json5';
 
-export type JSONReplacer = (key: string, value: unknown) => (number | string)[] | unknown | null;
-
 export type JSONReviver = (key: string, value: unknown) => unknown;
 
 export interface JSONStringifyOptions {
-  space?: number | string;
-  quote?: string;
-  replacer?: JSONReplacer;
+  space?: number | string | null;
+  quote?: string | null;
+  replacer?: (number | string)[] | ((key: string, value: unknown) => unknown) | null;
 }
 
 export function parse<T = object>(content: string, reviver?: JSONReviver): T {
   return JSON.parse(content, reviver);
 }
 
-export function stringify(content: unknown, options?: JSONStringifyOptions): string {
+export function stringify(content: unknown, options: JSONStringifyOptions = {}): string {
   return JSON.stringify(content, options);
 }
