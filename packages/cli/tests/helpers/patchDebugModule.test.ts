1a4875cc57706e3e3991b8f53d13eb7f877ecded
Miles Johnson <mileswjohnson@gmail.com>
2021-03-25 17:17:22 -0700

    Update tests.

---
+++ packages/cli/tests/helpers/patchDebugModule.test.ts
@@ -0,0 +1,21 @@
+import debug from 'debug';
+import patchDebugModule from '../../src/helpers/patchDebugModule';
+
+describe('patchDebugModule()', () => {
+  it('wraps `debug`', () => {
+    // We need to set env var before hand
+    process.env.DEBUG = '*';
+
+    const inst = debug('boostcli:test');
+    const spy = jest.spyOn(console, 'error').mockImplementation();
+    const unpatch = patchDebugModule();
+
+    debug.enable('boostcli:*');
+    inst('Debugging');
+
+    expect(spy).toHaveBeenCalledWith(expect.stringContaining('boostcli:test Debugging'));
+
+    unpatch();
+    spy.mockRestore();
+  });
+});
