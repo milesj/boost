d156ef2d90987c574656360017bff96c47323ea7
Mohamed El Mahallawy <mmahalwy@gmail.com>
2021-10-22 14:44:15 -0700

    feat: add input on work unit

--- packages/pipeline/src/WorkUnit.ts
+++ packages/pipeline/src/WorkUnit.ts
@@ -1,179 +1,182 @@
 import { Contract } from '@boost/common';
 import { BailEvent, Event } from '@boost/event';
 import {
-	STATUS_FAILED,
-	STATUS_PASSED,
-	STATUS_PENDING,
-	STATUS_RUNNING,
-	STATUS_SKIPPED,
+  STATUS_FAILED,
+  STATUS_PASSED,
+  STATUS_PENDING,
+  STATUS_RUNNING,
+  STATUS_SKIPPED,
 } from './constants';
 import { Context } from './Context';
 import { PipelineError } from './PipelineError';
 import { Action, Hierarchical, Runnable, Status } from './types';
 
 export abstract class WorkUnit<Options extends object, Input = unknown, Output = Input>
-	extends Contract<Options>
-	implements Runnable<Input, Output>, Hierarchical
+  extends Contract<Options>
+  implements Runnable<Input, Output>, Hierarchical
 {
-	depth: number = 0;
+  depth: number = 0;
 
-	index: number = 0;
+  index: number = 0;
 
-	output?: Output;
+  output?: Output;
 
-	startTime: number = 0;
+  input?: Input;
 
-	statusText: string = '';
+  startTime: number = 0;
 
-	stopTime: number = 0;
+  statusText: string = '';
 
-	/**
-	 * Called when an execution fails.
-	 * @category Events
-	 */
-	readonly onFail = new Event<[Error | null, Input]>('fail');
+  stopTime: number = 0;
 
-	/**
-	 * Called when an execution succeeds.
-	 * @category Events
-	 */
-	readonly onPass = new Event<[Output, Input]>('pass');
+  /**
+   * Called when an execution fails.
+   * @category Events
+   */
+  readonly onFail = new Event<[Error | null, Input]>('fail');
 
-	/**
-	 * Called before a work unit is executed. Can return `true` to skip the work unit.
-	 * @category Events
-	 */
-	readonly onRun = new BailEvent<[Input]>('run');
+  /**
+   * Called when an execution succeeds.
+   * @category Events
+   */
+  readonly onPass = new Event<[Output, Input]>('pass');
 
-	/**
-	 * Called when an execution is skipped.
-	 * @category Events
-	 */
-	readonly onSkip = new Event<[Input]>('skip');
+  /**
+   * Called before a work unit is executed. Can return `true` to skip the work unit.
+   * @category Events
+   */
+  readonly onRun = new BailEvent<[Input]>('run');
 
-	readonly title: string;
+  /**
+   * Called when an execution is skipped.
+   * @category Events
+   */
+  readonly onSkip = new Event<[Input]>('skip');
 
-	private action: Action<Context, Input, Output>;
+  readonly title: string;
 
-	private status: Status = STATUS_PENDING;
+  private action: Action<Context, Input, Output>;
 
-	// We want to support all contexts, so we use any.
-	// Unknown and `Context` will not work because of the constraint.
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	constructor(title: string, action: Action<any, Input, Output>, options?: Options) {
-		super(options);
+  private status: Status = STATUS_PENDING;
 
-		if (!title || typeof title !== 'string') {
-			throw new PipelineError('WORK_REQUIRED_TITLE');
-		}
+  // We want to support all contexts, so we use any.
+  // Unknown and `Context` will not work because of the constraint.
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  constructor(title: string, action: Action<any, Input, Output>, options?: Options) {
+    super(options);
 
-		if (action !== null && typeof action !== 'function') {
-			throw new PipelineError('ACTION_REQUIRED');
-		}
+    if (!title || typeof title !== 'string') {
+      throw new PipelineError('WORK_REQUIRED_TITLE');
+    }
 
-		this.action = action;
-		this.status = STATUS_PENDING;
-		this.title = title;
-	}
+    if (action !== null && typeof action !== 'function') {
+      throw new PipelineError('ACTION_REQUIRED');
+    }
 
-	/**
-	 * Return true if the task failed when executing.
-	 */
-	hasFailed(): boolean {
-		return this.status === STATUS_FAILED;
-	}
+    this.action = action;
+    this.status = STATUS_PENDING;
+    this.title = title;
+  }
 
-	/**
-	 * Return true if the task executed successfully.
-	 */
-	hasPassed(): boolean {
-		return this.status === STATUS_PASSED;
-	}
+  /**
+   * Return true if the task failed when executing.
+   */
+  hasFailed(): boolean {
+    return this.status === STATUS_FAILED;
+  }
 
-	/**
-	 * Return a unique hierarchical ID.
-	 */
-	get id() {
-		return `work[${this.depth}:${this.index}]`;
-	}
+  /**
+   * Return true if the task executed successfully.
+   */
+  hasPassed(): boolean {
+    return this.status === STATUS_PASSED;
+  }
 
-	/**
-	 * Return true if the task has been completed in any form.
-	 */
-	isComplete(): boolean {
-		return this.hasPassed() || this.hasFailed() || this.isSkipped();
-	}
+  /**
+   * Return a unique hierarchical ID.
+   */
+  get id() {
+    return `work[${this.depth}:${this.index}]`;
+  }
 
-	/**
-	 * Return true if the task has not been executed yet.
-	 */
-	isPending(): boolean {
-		return this.status === STATUS_PENDING;
-	}
+  /**
+   * Return true if the task has been completed in any form.
+   */
+  isComplete(): boolean {
+    return this.hasPassed() || this.hasFailed() || this.isSkipped();
+  }
 
-	/**
-	 * Return true if the task is currently running.
-	 */
-	isRunning(): boolean {
-		return this.status === STATUS_RUNNING;
-	}
+  /**
+   * Return true if the task has not been executed yet.
+   */
+  isPending(): boolean {
+    return this.status === STATUS_PENDING;
+  }
 
-	/**
-	 * Return true if the task was or will be skipped.
-	 */
-	isSkipped(): boolean {
-		return this.status === STATUS_SKIPPED;
-	}
+  /**
+   * Return true if the task is currently running.
+   */
+  isRunning(): boolean {
+    return this.status === STATUS_RUNNING;
+  }
 
-	/**
-	 * Run the current task by executing it and performing any before and after processes.
-	 */
-	async run(context: Context, value: Input): Promise<Output> {
-		const skip = this.onRun.emit([value]);
-		const runner: Action<Context, Input, Output> = this.action;
+  /**
+   * Return true if the task was or will be skipped.
+   */
+  isSkipped(): boolean {
+    return this.status === STATUS_SKIPPED;
+  }
 
-		if (skip || this.isSkipped() || !runner) {
-			this.status = STATUS_SKIPPED;
-			this.onSkip.emit([value]);
+  /**
+   * Run the current task by executing it and performing any before and after processes.
+   */
+  async run(context: Context, value: Input): Promise<Output> {
+    this.input = value;
+    const skip = this.onRun.emit([value]);
+    const runner: Action<Context, Input, Output> = this.action;
 
-			// Allow input as output. This is problematic for skipping
-			// since the expected output is no longer in sync. Revisit.
-			// @ts-expect-error Allow invalid type
-			return Promise.resolve(value);
-		}
+    if (skip || this.isSkipped() || !runner) {
+      this.status = STATUS_SKIPPED;
+      this.onSkip.emit([value]);
 
-		this.status = STATUS_RUNNING;
-		this.startTime = Date.now();
+      // Allow input as output. This is problematic for skipping
+      // since the expected output is no longer in sync. Revisit.
+      // @ts-expect-error Allow invalid type
+      return Promise.resolve(value);
+    }
 
-		try {
-			this.output = await runner(context, value, this);
-			this.status = STATUS_PASSED;
-			this.stopTime = Date.now();
-			this.onPass.emit([this.output, value]);
-		} catch (error: unknown) {
-			this.status = STATUS_FAILED;
-			this.stopTime = Date.now();
+    this.status = STATUS_RUNNING;
+    this.startTime = Date.now();
 
-			if (error instanceof Error) {
-				this.onFail.emit([error, value]);
+    try {
+      this.output = await runner(context, value, this);
+      this.status = STATUS_PASSED;
+      this.stopTime = Date.now();
+      this.onPass.emit([this.output, value]);
+    } catch (error: unknown) {
+      this.status = STATUS_FAILED;
+      this.stopTime = Date.now();
 
-				throw error;
-			}
-		}
+      if (error instanceof Error) {
+        this.onFail.emit([error, value]);
 
-		this.statusText = '';
+        throw error;
+      }
+    }
 
-		return this.output!;
-	}
+    this.statusText = '';
 
-	/**
-	 * Mark a task as skipped if the condition is true.
-	 */
-	skip(condition: boolean = true): this {
-		if (condition) {
-			this.status = STATUS_SKIPPED;
-		}
+    return this.output!;
+  }
 
-		return this;
-	}
+  /**
+   * Mark a task as skipped if the condition is true.
+   */
+  skip(condition: boolean = true): this {
+    if (condition) {
+      this.status = STATUS_SKIPPED;
+    }
+
+    return this;
+  }
 }
