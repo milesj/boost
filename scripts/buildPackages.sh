f385282422536f827c79364552adbd2c13465156
Miles Johnson <mileswjohnson@gmail.com>
2024-02-09 18:09:20 -0800

    Fix build order.

--- scripts/buildPackages.sh
+++ scripts/buildPackages.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
+yarn run clean
+
 # Packemon doesn't build packages in graph order,
 # so we must do this manually!
 
-packages=("internal" "module" "decorators" "debug" "event" "test-utils" "terminal" "common" "translate" "plugin" "pipeline" "log" "config" "args" "cli")
+packages=("internal" "module" "decorators" "event" "test-utils" "terminal" "common" "debug" "translate" "plugin" "pipeline" "log" "config" "args" "cli")
 
 for package in ${packages[@]}; do
 	echo "$package"
 	cd "./packages/$package" || exit
-	rm -rf {cjs,esm,mjs}
 	../../node_modules/.bin/packemon pack --addEngines --addExports --declaration
 	cd ../..
 done
