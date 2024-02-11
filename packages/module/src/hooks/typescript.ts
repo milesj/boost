cccd9c1f8d3b7a1af6a8b98125350f09f4eabdef
Miles Johnson <mileswjohnson@gmail.com>
2024-02-10 19:12:54 -0800

    breaking: Rewrite loaders into hooks.

--- packages/module/src/hooks/typescript.ts
+++ packages/module/src/hooks/typescript.ts
@@ -1,10 +1,10 @@
 // https://nodejs.org/api/esm.html#esm_loaders
 
 import fs from 'node:fs';
-import { LoaderLoad, LoaderResolve } from '../types';
+import type { InitializeHook, LoadHook, ResolveHook } from 'node:module';
+import { fileURLToPath } from 'node:url';
 import {
 	COMPILER_OPTIONS,
-	getModuleFormat,
 	getModuleFromNodeVersion,
 	getTargetFromNodeVersion,
 	isNodeNext,
@@ -36,7 +36,7 @@ async function transform(url: string, source: string): Promise<string> {
 			module: getModuleFromNodeVersion(ts, nodeNext),
 			moduleResolution: nodeNext
 				? ts.ModuleResolutionKind.NodeNext
-				: ts.ModuleResolutionKind.NodeJs,
+				: ts.ModuleResolutionKind.Node16,
 			resolveJsonModule: false,
 			target: getTargetFromNodeVersion(ts),
 		},
@@ -44,9 +44,12 @@ async function transform(url: string, source: string): Promise<string> {
 	}).outputText;
 }
 
-export const resolve: LoaderResolve = async (specifier, context, defaultResolve) => {
+export const initialize: InitializeHook = () => {};
+
+export const resolve: ResolveHook = (specifier, context, nextResolve) => {
 	if (isTypeScript(specifier)) {
 		return {
+			shortCircuit: true,
 			url: new URL(specifier, context.parentURL).href,
 		};
 	}
@@ -58,30 +61,27 @@ export const resolve: LoaderResolve = async (specifier, context, defaultResolve)
 
 			if (fs.existsSync(url)) {
 				return {
+					format: 'module',
+					shortCircuit: true,
 					url: url.href,
 				};
 			}
 		}
 	}
 
-	return defaultResolve(specifier, context);
+	return nextResolve(specifier, context);
 };
 
-export const load: LoaderLoad = async (url, context, defaultLoad) => {
+export const load: LoadHook = async (url, context, nextLoad) => {
 	if (isTypeScript(url)) {
-		const format = getModuleFormat(url);
-
-		if (format === 'commonjs') {
-			return { format };
-		}
-
-		const { source: rawSource } = await defaultLoad(url, { format });
+		const source = await transform(url, await fs.promises.readFile(fileURLToPath(url), 'utf8'));
 
 		return {
-			format,
-			source: await transform(url, String(rawSource)),
+			format: 'module',
+			shortCircuit: true,
+			source,
 		};
 	}
 
-	return defaultLoad(url, context);
+	return nextLoad(url, context);
 };
