036eb3d0419052435a7fc7255a9262b17ab85fa3
Miles Johnson <mileswjohnson@gmail.com>
2024-02-10 17:27:27 -0800

    internal: Replace __dirname and __filename.

--- packages/log/src/translate.ts
+++ packages/log/src/translate.ts
@@ -1,4 +1,8 @@
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { createTranslator } from '@boost/translate';
 
-export const msg = createTranslator('log', path.join(__dirname, '../res'));
+export const msg = createTranslator(
+	'log',
+	path.join(path.dirname(fileURLToPath(import.meta.url)), '../res'),
+);
--- packages/module/tests/helpers.ts
+++ packages/module/tests/helpers.ts
@@ -1,5 +1,6 @@
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 export function getFixture(file: string): string {
-	return path.join(__dirname, '__fixtures__', file);
+	return path.join(path.dirname(fileURLToPath(import.meta.url)), '__fixtures__', file);
 }
